4474dd320ed893f9502cd9d11571261835907241
n.x <zkolsh@proton.me>
2025-09-19 13:09:53 -0300

    estandarizar indentacion

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 #include "generador_primos.h"
 
 int main() {
-  // Genero y muestro los primeros 100 números primos
-  int *p = primos(100);
-  for(int i = 0; i < 100; i++)
-    printf("%d ",p[i]);
+	// Genero y muestro los primeros 100 números primos
+	int *p = primos(100);
+	for(int i = 0; i < 100; i++)
+		printf("%d ",p[i]);
 
-  // Uso los números primos
+	// Uso los números primos
 
-  // IMPORTANTE: libero la memoria pedida para el arreglo de números primos
-  free(p);
-  return 0;
+	// IMPORTANTE: libero la memoria pedida para el arreglo de números primos
+	free(p);
+	return 0;
 }
