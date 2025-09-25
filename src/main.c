051bd81a5cae8de7f47f8286cae7d42beaa6ae56
n.x <zkolsh@proton.me>
2025-09-24 23:47:48 -0300

    mas pruebas

--- src/main.c
+++ src/main.c
@@ -38,6 +38,42 @@ int main() {
 	Blockchain* bl_felipe = bl_agregar_inicio(NULL, deuda_felipe.tiempo, deuda_felipe.longitud, deuda_felipe.mensaje);
 	size_t id_felipe = merkle_alta(arbol_deudores, bl_felipe);
 
+	Registro deuda_mate = registrar_deuda(2, "Mateo Delmagro", 98765432, 120000);
+	Blockchain* bl_mate = bl_agregar_inicio(bl_felipe, deuda_mate.tiempo, deuda_mate.longitud, deuda_mate.mensaje);
+	size_t id_mate = merkle_alta(arbol_deudores, bl_mate);
+
+	Registro deuda_manu = registrar_deuda(3, "Manuel Slepoy", 11223344, 45000);
+	Blockchain* bl_manu = bl_agregar_inicio(bl_mate, deuda_manu.tiempo, deuda_manu.longitud, deuda_manu.mensaje);
+	size_t id_manu = merkle_alta(arbol_deudores, bl_manu);
+
+	if (merkle_validar(arbol_deudores)) {
+		printf("Árbol Merkle válido.\n");
+	} else {
+		printf("Árbol Merkle inválido.\n");
+	};
+
+	Registro pago_manu = registrar_credito(4, "Manuel Slepoy", 11223344, 20000);
+	merkle_amendar(arbol_deudores, id_manu, id_manu, pago_manu.longitud, pago_manu.mensaje);
+
+	size_t producto_esperado = arbol_deudores->datos[id_felipe] * arbol_deudores->datos[id_mate];
+	if (merkle_validar_subconjunto(arbol_deudores, producto_esperado, id_felipe, id_mate)) {
+		printf("Subconjunto válido entre Felipe y Mate.\n");
+	} else {
+		printf("Subconjunto inválido.\n");
+	};
+
+	producto_esperado = arbol_deudores->datos[id_mate] * arbol_deudores->datos[id_manu];
+	if (merkle_validar_subconjunto(arbol_deudores, producto_esperado, id_mate, id_manu)) {
+		printf("Subconjunto válido entre Mateo y Manuel.\n");
+	} else {
+		printf("Subconjunto inválido.\n");
+	};
+
+	free(deuda_felipe.mensaje);
+	free(deuda_mate.mensaje);
+	free(deuda_manu.mensaje);
+	free(pago_manu.mensaje);
+	bl_destruir(bl_manu);
 	merkle_destruir(arbol_deudores);
 	return 0;
 };
