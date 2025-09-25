3c8f7983dd312a87f27bbeb9fdb583dd738087ab
n.x <zkolsh@proton.me>
2025-09-24 23:47:59 -0300

    corregir problemas de memoria

--- src/merkle.c
+++ src/merkle.c
@@ -2,12 +2,24 @@
 
 Merkle* merkle_crear(size_t cant_nodos, Blockchain** nodos) {
 	Merkle* arbol = malloc(sizeof(Merkle));
-	arbol->capacidad = MERKLE_CAPACIDAD_MINIMA;
-	arbol->tamaño = cant_nodos;
-	arbol->datos = malloc(arbol->capacidad * sizeof(unsigned long long));
-	arbol->primos = primos((int)arbol->capacidad);
-	arbol->nodos = nodos;
+	arbol->capacidad_datos = MERKLE_CAPACIDAD_MINIMA;
+	arbol->datos = malloc(arbol->capacidad_datos * sizeof(unsigned long long));
+	arbol->longitud_primos = arbol->capacidad_datos;
+	arbol->indice_primos = 0;
+	arbol->primos = primos((int)arbol->capacidad_datos);
+
+	if (cant_nodos < MERKLE_CAPACIDAD_MINIMA) {
+		cant_nodos = MERKLE_CAPACIDAD_MINIMA;
+	};
+
+	arbol->nodos = (Blockchain**)malloc(cant_nodos * sizeof(Blockchain*));
 	arbol->cantidad_nodos = cant_nodos;
+	arbol->capacidad_nodos = cant_nodos;
+
+	if (nodos) {
+		memcpy((void*)arbol->nodos, (void*)nodos, cant_nodos * sizeof(Blockchain*));
+	};
+
 	return arbol;
 };
 
@@ -15,12 +27,13 @@ void merkle_destruir(Merkle* arbol) {
 	if (!arbol) return;
 	if (arbol->primos) free(arbol->primos);
 	if (arbol->datos) free(arbol->datos);
+	if (arbol->nodos) free((void*)arbol->nodos);
 	free(arbol);
 };
 
-void merkle_realloc(Merkle* arbol, size_t nueva_capacidad) {
+void merkle_realloc_datos(Merkle* arbol, size_t nueva_capacidad) {
 	if (!arbol) return;
-	if (nueva_capacidad <= arbol->capacidad) return;
+	if (nueva_capacidad <= arbol->capacidad_datos) return;
 
 	unsigned long long* nuevos_datos = realloc(arbol->datos, sizeof(unsigned long long) * nueva_capacidad);
 	if (!nuevos_datos) {
@@ -29,15 +42,33 @@ void merkle_realloc(Merkle* arbol, size_t nueva_capacidad) {
 	};
 
 	arbol->datos = nuevos_datos;
-	arbol->capacidad = nueva_capacidad;
+	arbol->capacidad_datos = nueva_capacidad;
+};
+
+void merkle_realloc_nodos(Merkle* arbol, size_t nueva_capacidad) {
+	if (!arbol) return;
+	if (nueva_capacidad <= arbol->capacidad_nodos) return;
+
+	Blockchain** nuevos_nodos = (Blockchain**)realloc((void*)arbol->nodos, sizeof(Blockchain*) * nueva_capacidad);
+	if (!nuevos_nodos) {
+		perror("no se pudo hacer realloc\n");
+		exit(1);
+	};
+
+	arbol->nodos = nuevos_nodos;
+	arbol->capacidad_nodos = nueva_capacidad;
 };
 
 size_t merkle_alta(Merkle* arbol, Blockchain *nodo) {
 	if (!arbol) return 0;
 	if (!nodo) return 0;
 
-	if (arbol->tamaño >= arbol->capacidad) {
-		merkle_realloc(arbol, arbol->capacidad * 2);
+	if (arbol->cantidad_nodos >= arbol->capacidad_datos / 2) {
+		merkle_realloc_datos(arbol, arbol->capacidad_datos * 2);
+	};
+
+	if (arbol->cantidad_nodos + 1 >= arbol->capacidad_nodos) {
+		merkle_realloc_nodos(arbol, arbol->capacidad_nodos * 2);
 	};
 
 	if (arbol->indice_primos >= arbol->longitud_primos) {
@@ -46,26 +77,27 @@ size_t merkle_alta(Merkle* arbol, Blockchain *nodo) {
 		arbol->primos = primos((int)arbol->longitud_primos);
 	};
 
-	arbol->datos[arbol->tamaño + 1] = arbol->primos[arbol->indice_primos++];
-	size_t i = (arbol->tamaño + 1) / 2;
+	size_t tamaño = arbol->cantidad_nodos + (arbol->capacidad_datos / 2);
+	arbol->datos[tamaño] = arbol->primos[arbol->indice_primos++];
+	arbol->nodos[arbol->cantidad_nodos] = nodo;
+	size_t i = tamaño / 2;
 
-	arbol->tamaño++;
 	arbol->cantidad_nodos++;
 	while (i != 1) {
 		size_t idx_izq = i * 2;
 		size_t idx_der = i * 2 + 1;
 
-		unsigned long long izq = (idx_izq > arbol->tamaño)? 1 : arbol->datos[idx_izq];
-		unsigned long long der = (idx_der > arbol->tamaño)? 1 : arbol->datos[idx_der];
+		unsigned long long izq = (idx_izq > tamaño)? 1 : arbol->datos[idx_izq];
+		unsigned long long der = (idx_der > tamaño)? 1 : arbol->datos[idx_der];
 		arbol->datos[i] = izq * der;
 		i /= 2;
 	};
 
-	return arbol->tamaño - (arbol->capacidad / 2);
+	return tamaño - (arbol->capacidad_datos / 2);
 };
 
 void merkle_actualizar(Merkle* arbol, size_t id_blockchain, size_t id_nodo, size_t long_mensaje, char* mensaje) {
-	if(arbol == NULL || id_blockchain > arbol->tamaño)
+	if(arbol == NULL || id_blockchain > arbol->cantidad_nodos)
 		return;
 
 	Blockchain* indice_bl = arbol->nodos[id_blockchain];
@@ -87,23 +119,24 @@ void merkle_actualizar(Merkle* arbol, size_t id_blockchain, size_t id_nodo, size
 		arbol->primos = primos((int)arbol->longitud_primos);
 	};
 
-	size_t indice_mk = (arbol->capacidad / 2) + id_nodo;
+	size_t indice_mk = (arbol->capacidad_datos / 2) + id_nodo;
 	arbol->datos[indice_mk] = arbol->primos[arbol->indice_primos++];
 	indice_mk /= 2;
 
+	size_t tamaño = arbol->cantidad_nodos + (arbol->capacidad_datos / 2);
 	while (indice_mk != 1) {
 		size_t idx_izq = indice_mk * 2;
 		size_t idx_der = indice_mk * 2 + 1;
 
-		unsigned long long izq = (idx_izq > arbol->tamaño)? 1 : arbol->datos[idx_izq];
-		unsigned long long der = (idx_der > arbol->tamaño)? 1 : arbol->datos[idx_der];
+		unsigned long long izq = (idx_izq > tamaño)? 1 : arbol->datos[idx_izq];
+		unsigned long long der = (idx_der > tamaño)? 1 : arbol->datos[idx_der];
 		arbol->datos[indice_mk] = izq * der;
 		indice_mk /= 2;
 	};
 }
 
 void merkle_amendar(Merkle* arbol, size_t id_blockchain, size_t id_nodo, size_t long_mensaje, char* mensaje) {
-	if(arbol == NULL || id_blockchain > arbol->tamaño)
+	if(arbol == NULL || id_blockchain > arbol->cantidad_nodos)
 		return;
 
 	Blockchain* indice_bl = arbol->nodos[id_blockchain];
@@ -115,16 +148,17 @@ void merkle_amendar(Merkle* arbol, size_t id_blockchain, size_t id_nodo, size_t 
 		arbol->primos = primos((int)arbol->longitud_primos);
 	};
 
-	size_t indice_mk = (arbol->capacidad / 2) + id_nodo;
+	size_t indice_mk = (arbol->capacidad_datos / 2) + id_nodo;
 	arbol->datos[indice_mk] = arbol->primos[arbol->indice_primos++];
 	indice_mk /= 2;
 
+	size_t tamaño = arbol->cantidad_nodos + (arbol->capacidad_datos / 2);
 	while (indice_mk != 1) {
 		size_t idx_izq = indice_mk * 2;
 		size_t idx_der = indice_mk * 2 + 1;
 
-		unsigned long long izq = (idx_izq > arbol->tamaño)? 1 : arbol->datos[idx_izq];
-		unsigned long long der = (idx_der > arbol->tamaño)? 1 : arbol->datos[idx_der];
+		unsigned long long izq = (idx_izq > tamaño)? 1 : arbol->datos[idx_izq];
+		unsigned long long der = (idx_der > tamaño)? 1 : arbol->datos[idx_der];
 		arbol->datos[indice_mk] = izq * der;
 		indice_mk /= 2;
 	};
@@ -132,9 +166,9 @@ void merkle_amendar(Merkle* arbol, size_t id_blockchain, size_t id_nodo, size_t 
 
 unsigned long long merkle_validar_subarbol(Merkle* arbol, size_t id_nodo) {
 	if (!arbol) return 0;
-	if (id_nodo > arbol->capacidad) return 0;
-	if (id_nodo > arbol->tamaño) return 1;
-	if (id_nodo > arbol->capacidad / 2) return arbol->datos[id_nodo];
+	if (2 * id_nodo > arbol->capacidad_datos) return 0;
+	if (id_nodo > arbol->cantidad_nodos) return 1;
+	if (id_nodo > arbol->capacidad_datos / 2) return arbol->datos[id_nodo];
 	unsigned long long izq = merkle_validar_subarbol(arbol, id_nodo * 2);
 	unsigned long long der = merkle_validar_subarbol(arbol, id_nodo * 2 + 1);
 	unsigned long long valor_esperado = izq * der;
@@ -166,7 +200,7 @@ bool merkle_validar_subconjunto(Merkle* arbol, size_t producto_esperado, size_t 
 	unsigned long long producto = 1;
 
 	for(size_t i=inicio; i<fin; i++){
-		producto *= arbol->datos[(arbol->capacidad / 2) + i];
+		producto *= arbol->datos[(arbol->capacidad_datos / 2) + i];
 
 		Blockchain* lista = arbol->nodos[i];
 		if(lista == NULL) continue;
--- src/merkle.h
+++ src/merkle.h
@@ -12,21 +12,23 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Merkle {
-	size_t capacidad;
-	size_t tamaño;
+	size_t capacidad_datos;
 	unsigned long long* datos;
 	size_t longitud_primos;
 	size_t indice_primos;
 	int* primos;
 	size_t cantidad_nodos;
+	size_t capacidad_nodos;;
 	Blockchain** nodos;
 } Merkle;
 
 Merkle* merkle_crear(size_t cant_nodos, Blockchain** nodos);
 void merkle_destruir(Merkle* arbol);
-void merkle_realloc(Merkle* arbol, size_t nueva_capacidad);
+void merkle_realloc_datos(Merkle* arbol, size_t nueva_capacidad);
+void merkle_realloc_nodos(Merkle* arbol, size_t nueva_capacidad);
 size_t merkle_alta(Merkle* arbol, Blockchain* nodo);
 void merkle_actualizar(Merkle* arbol, size_t id_blockchain, size_t id_nodo, size_t long_mensaje, char* mensaje);
 void merkle_amendar(Merkle* arbol, size_t id_blockchain, size_t id_nodo, size_t long_mensaje, char* mensaje);
